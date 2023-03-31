18ed55c2e0ef16a4a1cd85c79a2bb3a83293e028
Surajy001 <soorajy001@gmail.com>
2023-04-01 05:04:29 +0530

    day-4 Sort & Filter Function

--- src/Redux/Products_Reducer/action.js
+++ src/Redux/Products_Reducer/action.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 import { GET_MEN_PRODUCTS_SUCCESS, GET_WOMEN_PRODUCTS_SUCCESS,PRODUCTS_FAILURE, PRODUCTS_REQUEST ,GET_SINGLE_PRODUCT_SUCCESS} from "./actionTypes"
 const URL="http://localhost:8080"
+//const URL="https://deployed-server-byloki.onrender.com"
 
 
-export const getMenProducts=()=>(dispatch)=>{
+export const getMenProducts=(Obj)=>(dispatch)=>{
   dispatch({type:PRODUCTS_REQUEST});
-  axios.get(`${URL}/men`)
+  axios.get(`${URL}/men`,Obj)
   .then((res)=>{
     dispatch({type:GET_MEN_PRODUCTS_SUCCESS , payload:res.data})
   }).catch(()=>{
@@ -15,9 +16,9 @@ export const getMenProducts=()=>(dispatch)=>{
 
 };
 
-export const getWoMenProducts=()=>(dispatch)=>{
+export const getWoMenProducts=(Obj)=>(dispatch)=>{
   dispatch({type:PRODUCTS_REQUEST});
-  axios.get(`${URL}/women`)
+  axios.get(`${URL}/women`,Obj)
   .then((res)=>{
     dispatch({type:GET_WOMEN_PRODUCTS_SUCCESS , payload:res.data})
   }).catch(()=>{
