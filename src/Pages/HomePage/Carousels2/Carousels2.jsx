9f1e9fa31ed1b4a7904748fefbcf9e20f8ba0432
Loki343 <kausikbhaumik42@gmail.com>
2023-05-15 12:58:02 +0530

    resolved 404 error on homepage items

--- src/Pages/HomePage/Carousels2/Carousels2.jsx
+++ src/Pages/HomePage/Carousels2/Carousels2.jsx
@@ -57,49 +57,49 @@ export function Carousels2() {
             scrollBehavior: "smooth",
           }}
         >
-          <Link to="/">
+          <Link to="/men-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/93FB0DD4-CC1E-4F10-8A59-975E389BA547?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Coats & Jackets"
             />
           </Link>
-          <Link to="/">
+          <Link to="/men-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/D669222C-897A-4D72-8C1F-6FA3FFAE5883?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Bag"
             />
           </Link>
-          <Link to="/">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/9C2BA2E0-9207-421B-9C61-712A7F06C1EF?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Shoes"
             />
           </Link>
-          <Link to="/">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/576346F0-CF31-4C7E-BD52-55FD0144E429?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Accessories"
             />
           </Link>
-          <Link to="/">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/A9C10572-B14D-4FF7-9732-07346606BA16?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Clothing"
             />
           </Link>
-          <Link to="/">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/5C61DA59-09FB-4F37-8C38-47FE852B2E1C?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Baby"
             />
           </Link>
-          <Link to="/store?filter=Entertainment">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/B9A0EE11-0A57-4AEB-B421-F00B9263415E?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Fragrance"
             />
           </Link>
-          <Link to="/">
+          <Link to="/women-product">
             <MyCard
               src="https://assets.burberry.com/is/image/Burberryltd/DB721713-E620-41A6-9CF6-C3504A5F5B82?$BBY_V2_SL_1x1$&wid=1251&hei=1251"
               cap="Gifts"
